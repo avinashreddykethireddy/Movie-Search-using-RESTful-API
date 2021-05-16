8c0cc7ed9b6ef5d36ac310d1e51e545a1e4082d3
Avinash Kethireddy <avinashkethireddy.reddy@gmail.com>
2021-05-16 21:04:22 +0530

    done seraching

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-
+var request = require('request');
 
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname, 'public')));
 app.get("/",function(req,res){
     res.render("home");
 });
+app.get("/result",function(req,res){
+    //console.log(req.query.movie_name);
+    var query = req.query.movie_name;
+    var html_link = "http://www.omdbapi.com/?s="+query+"&apikey=thewdb"; 
+    request(html_link,function(error,response,body){
+        if(!error && response.statusCode == 200){
+            
+            var parsedData = JSON.parse(body);
+            console.log(parsedData);
+            if(parsedData['Response'] === "False"){
+                res.send("NO PAGE!");
+            }
+            else{        
+                res.render("result",{parsedData:parsedData});
+            }
+        }
+    });
+});
+
 
 
 // app.listen(process.env.PORT,process.env.IP);
---
+++ public/javascripts/main.js
@@ -0,0 +1,7 @@
+// alert("hello");
+
+// $("ul").on("click","li",function(){
+// 	// $(this).css("color","gray");
+// 	// $(this).css("text-decoration","line-through");
+// 	$(this).toggleClass("active");
+// });
