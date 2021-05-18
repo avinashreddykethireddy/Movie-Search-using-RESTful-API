fd371b65f0ec14fa2f3c8b611bb68de6d5a08e28
Avinash Kethireddy <avinashkethireddy.reddy@gmail.com>
2021-05-18 11:25:07 +0530

    added moreinfo page

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ var request = require('request');
 
 app.set("view engine","ejs");
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.get("/",function(req,res){
     res.render("home");
 });
 app.get("/result",function(req,res){
     //console.log(req.query.movie_name);
     var query = req.query.movie_name;
-    var html_link = "http://www.omdbapi.com/?s="+query+"&apikey=thewdb"; 
+    var html_link = "http://www.omdbapi.com/?s="+query+"&apikey=a9de43cc"; 
     request(html_link,function(error,response,body){
         if(!error && response.statusCode == 200){
             
@@ -22,6 +23,24 @@ app.get("/result",function(req,res){
             }
             else{        
                 res.render("result",{parsedData:parsedData});
+            }
+        }
+    });
+});
+
+app.get("/moreinfo",function(req,res){
+    var query = req.query.imdbID;
+    var html_link = "http://www.omdbapi.com/?i="+query+"&apikey=a9de43cc"; 
+    request(html_link,function(error,response,body){
+        if(!error && response.statusCode == 200){
+            
+            var parsedData = JSON.parse(body);
+            console.log(parsedData);
+            if(parsedData['Response'] === "False"){
+                res.send("NO PAGE!");
+            }
+            else{        
+                res.render("moreinfo",{parsedData:parsedData});
             }
         }
     });
