5f05a66989fca492b6a2a81b0a25d0b53ceeb4f3
Avinash Kethireddy <avinashkethireddy.reddy@gmail.com>
2021-05-18 13:45:51 +0530

    way to final

--- public/javascripts/main.js
+++
@@ -1,7 +0,0 @@
-// alert("hello");
-
-// $("ul").on("click","li",function(){
-// 	// $(this).css("color","gray");
-// 	// $(this).css("text-decoration","line-through");
-// 	$(this).toggleClass("active");
-// });
